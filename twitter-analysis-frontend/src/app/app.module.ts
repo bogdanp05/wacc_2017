ec4abb4334daa592d690a978b012a1c5d6b3aadc
Bogdan Petre <bogdan.petre0510@gmail.com>
2017-09-26 11:02:21 +0200

    Some refactoring

--- twitter-analysis-frontend/src/app/app.module.ts
+++ twitter-analysis-frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TweetsListComponent } from './tweets-list/tweets-list.component';
-import { FilterButtonsComponent } from './filter-buttons/filter-buttons.component';
 import { TweetsValuesCounterComponent } from './tweets-values-counter/tweets-values-counter.component';
 import { TweetService } from './tweet.service';
 
@@ -18,7 +17,6 @@ import { InMemoryDataService }  from './in-memory-data.service';
   declarations: [
     AppComponent,
     TweetsListComponent,
-    FilterButtonsComponent,
     TweetsValuesCounterComponent
   ],
   imports: [
