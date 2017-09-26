3abece11065e99f6994d3a8727132bcf4f72e50c
Bogdan Petre <bogdan.petre0510@gmail.com>
2017-09-26 10:45:39 +0200

    Implemented http calls

--- twitter-analysis-frontend/src/app/app.module.ts
+++ twitter-analysis-frontend/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpModule }    from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { TweetsListComponent } from './tweets-list/tweets-list.component';
 import { FilterButtonsComponent } from './filter-buttons/filter-buttons.component';
 import { TweetsValuesCounterComponent } from './tweets-values-counter/tweets-values-counter.component';
+import { TweetService } from './tweet.service';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -22,9 +25,10 @@ import { InMemoryDataService }  from './in-memory-data.service';
     BrowserModule,
     NgbModule.forRoot(),
     FlexLayoutModule,
+    HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
-  providers: [],
+  providers: [TweetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- twitter-analysis-frontend/src/app/tweet.service.ts
+++ twitter-analysis-frontend/src/app/tweet.service.ts
@@ -1,11 +1,21 @@
 import { Tweet } from './tweet';
-import { TWEETS } from './mock-tweets';
+// import { TWEETS } from './mock-tweets';
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class TweetService {
+  private tweetsUrl = 'api/tweets';
+  constructor(private http: Http) { }
+
   getTweets(): Promise<Tweet[]> {
-    return Promise.resolve(TWEETS);
+    // return Promise.resolve(TWEETS);
+    return this.http.get(this.tweetsUrl)
+          .toPromise()
+          .then(response => response.json().data as Tweet[])
+          .catch(this.handleError);
   }
 
   // getHeroesSlowly(): Promise<Hero[]> {
@@ -15,13 +25,23 @@ export class TweetService {
   //   });
   // }
 
-  getTweet(id: number): Promise<Tweet> {
-    return this.getTweets()
-               .then(tweets => tweets.find(tweet => tweet.id === id));
-  }
+  // getTweet(id: number): Promise<Tweet> {
+  //   return this.getTweets()
+  //              .then(tweets => tweets.find(tweet => tweet.id === id));
+  // }
 
   filterTweets(quality: number): Promise<Tweet[]>{
-    return this.getTweets()
-               .then(tweets => tweets.filter(tweet => tweet.analysis=== quality));
+    // return this.getTweets()
+    //            .then(tweets => tweets.filter(tweet => tweet.analysis=== quality));
+    return this.http.get(this.tweetsUrl)
+          .toPromise()
+          .then(response => response.json().data as Tweet[])
+          .then(tweets => tweets.filter(tweet => tweet.analysis=== quality))
+          .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
