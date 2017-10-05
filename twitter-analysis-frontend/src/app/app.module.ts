d15a8c6fb7fba9f16b2a65d8f17af24e0280c4f4
Yuki_Kan <yukikan22@gmail.com>
2017-10-06 01:10:11 +0200

    Add line chart

--- twitter-analysis-frontend/src/app/app.module.ts
+++ twitter-analysis-frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpModule } from '@angular/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { TweetsValuesCounterComponent } from './tweets-values-counter/tweets-values-counter.component';
@@ -10,10 +11,11 @@ import { TweetService } from './tweet.service';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { FilterButtonsComponent } from './filter-buttons/filter-buttons.component';
 import { TweetsTableComponent } from './tweets-table/tweets-table.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +23,15 @@ import { TweetsTableComponent } from './tweets-table/tweets-table.component';
     TweetsValuesCounterComponent,
     SearchBarComponent,
     FilterButtonsComponent,
-    TweetsTableComponent
+    TweetsTableComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     FlexLayoutModule,
     HttpModule,
+    ChartsModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService, {passThruUnknownUrl: true}),
   ],
   providers: [TweetService],
